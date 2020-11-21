abae81dd8e49fe53cf2abe026d779932cab8a2e7
NipunRautela <nipunrautela261@gmail.com>
2020-11-21 10:59:59 +0530

    Base app for blog app

---
+++ lib/views/home.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:blog_app/views/create_blog.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Blog App"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => CreateBlog()));
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
